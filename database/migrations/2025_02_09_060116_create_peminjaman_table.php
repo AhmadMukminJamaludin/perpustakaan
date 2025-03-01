27642f8122b4716021f24fe2336920516bf941d9
AhmadMukminJamaludin <jcfirda@gmail.com>
2025-03-01 20:47:59 +0700

    Change buku_id column type to UUID in peminjaman table migration

--- database/migrations/2025_02_09_060116_create_peminjaman_table.php
+++ database/migrations/2025_02_09_060116_create_peminjaman_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('peminjaman', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->string('buku_id', 36);
+            $table->uuid('buku_id');
             $table->foreign('buku_id')->references('id')->on('buku')->onDelete('cascade');
             $table->date('tanggal_pinjam');
             $table->date('tanggal_kembali')->nullable();
